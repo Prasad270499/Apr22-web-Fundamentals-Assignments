eba5f7f028465792a1e219e98b5f9484b3bbe42c
Prasad27041999 <prasadhirwe72@gmail.com>
2022-06-12 17:41:39 +0530

    c

--- June/Week2/Sunday Test/Question 2/index.js
+++ June/Week2/Sunday Test/Question 2/index.js
@@ -1,12 +1,12 @@
 //  methodes for looping an array
-//  1. for loop simple methode : 
+//  1. for loop simple method: 
 //  example
 let array = [1,2,3,4,5,6];
 for(let i=0;i<array.length;i++){ 
     console.log('methode1',array[i]); 
 }
 console.log('------------');
-// 2. for loop of methode : in this methode we use for loop but this time simply saying <variable name> of <array name>
+// 2. for loop of method: in this methode we use for loop but this time simply saying <variable name> of <array name>
 //  example
 for(let elements of array){
     console.log('methode2',elements);
